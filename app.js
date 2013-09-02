e03b23eac6df2e666978fd9b6e9544a24a884e69
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-09-02 11:29:22 +0200

    locked port

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
- var express = require('express');
-  var app = express();
+var express = require('express');
+var app = express();
 
-  app.get('/', function(req, res){
-    res.send("Hello World!")
-  });
+app.get('/', function(req, res){
+  res.send("Hello World!")
+});
 
-  app.get('/clouds.json', function(req, res){
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({clous : ["Altocumulus ", "Altostratus", "Cumulonimbus", "Nimbostratus", "Cirrocumulus", "Stratus"]}));
-    res.end();
-  });
+app.get('/clouds.json', function(req, res){
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify({clous : ["Altocumulus ", "Altostratus", "Cumulonimbus", "Nimbostratus", "Cirrocumulus", "Stratus"]}));
+  res.end();
+});
 
-  var port = process.env.PORT || 5000;
-  app.listen(port);
+var port = 5000;
+app.listen(port);
 
-  module.exports = app;
+module.exports = app;
