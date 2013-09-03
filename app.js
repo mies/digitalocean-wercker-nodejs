888926300f2de5bf8e070f616642d2069421a9bc
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-09-03 11:19:05 +0200

    added unit test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send("Hello World!")
+  res.send("Hello from digital ocean and wercker!")
 });
 
 app.get('/clouds.json', function(req, res){
